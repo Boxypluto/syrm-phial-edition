715a83f8f30a73e33a8060730cb7f97767b2e4c7
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2025-09-23 22:53:24 -0400

    So much Sequence Rework. And INSANE amounts of RegEx

--- content/component/music_seqence.gd
+++ content/component/music_seqence.gd
@@ -1,13 +1,15 @@
 extends Resource
 class_name Seqence
 
-const PATTERN_REGEX: String = "(?<repitition>\\d*)(?<pattern>\\[(?:(?:[^][]*|(?R))*)\\])"
-const NOTE_REGEX: String = "[A-G]((?<=[ACDFG])#)?\\d*|S"
+#const PATTERN_REGEX: String = "(?<repitition>\\d*)(?:\\[(?<pattern>(?:[^][]*|(?R))*)\\])"
+#const NOTE_REGEX: String = "/(?<note>(?<note_length>(?:\\d+\\.?\\d*|\\.\\d+))?(?<note_pitch>[A-G](?:(?<=[ACDFG])#)?)(?<note_octave>(?:-(?=\\d))?\\d+?)?)|(?<pattern>S)|(?<rest_length>(?:\\d+\\.?\\d*|\\.\\d+))?(?<rest>-)/gm"
+const NOTES_AND_PATTERNS_REGEX = "(?:(?<pattern_multiplier>(?:\\d+\\.?\\d*|\\.\\d+))x)?(?<pattern_repitition>\\d+)?(?:\\[(?<pattern>(?:[^][]*|(?R))*)\\])(?<pattern_octave>(?:-(?=\\d))?\\d+)?|(?<note>(?<note_length>(?:\\d+\\.?\\d*|\\.\\d+))?(?<note_pitch>[A-G](?:(?<=[ACDFG])#)?)(?<note_octave>(?:-(?=\\d))?\\d+)?)|(?<rest_length>(?:\\d+\\.?\\d*|\\.\\d+))?(?<rest>-)"
 const TRIM_EXTRA_SPACE_REGEX = "(?<= )\\s+|\\v+|( +$)|(^ +)"
-const MULTIPLIER_REGEX: String = "^x(\\d+\\.?\\d*|\\.\\d+)"
+const MULTIPLIER_REGEX: String = "(?:^X(?<multiplier>\\d+\\.?\\d*|\\.\\d+))"
 
-static var patterns: RegEx = RegEx.create_from_string(PATTERN_REGEX)
-static var notes: RegEx = RegEx.create_from_string(NOTE_REGEX)
+#static var patterns: RegEx = RegEx.create_from_string(PATTERN_REGEX)
+#static var notes: RegEx = RegEx.create_from_string(NOTE_REGEX)
+static var patterns_and_notes: RegEx = RegEx.create_from_string(NOTES_AND_PATTERNS_REGEX)
 static var trimmed: RegEx = RegEx.create_from_string(TRIM_EXTRA_SPACE_REGEX)
 static var multiplier: RegEx = RegEx.create_from_string(MULTIPLIER_REGEX)
 
@@ -21,14 +23,64 @@ static func build(seqences: Array[String]):
 	var sequence: Seqence = Seqence.new()
 	sequence.tracks = seqences.size()
 
-static var example = "x0.5 A B C 3[A B] [A B C] [E A# [D# C [B C C A C] [A A A]]]"
+static var example = "/[A B]"
 static var digits: PackedStringArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-class SequencePart:
-	pass
+class SequenceTrack:
+	var length_multiplier: float = 1.0
+	
 
 static func _build_single(seqence_string: String):
-	var multiplier_match: PackedStringArray = multiplier.search(seqence_string).strings
-	var multiplier_number: float = multiplier.search(seqence_string).strings[0].substr(1).to_float() if not multiplier_match.is_empty() else 1.0
-	var cleaned: String = trimmed.sub(seqence_string, "", true)
-	print(multiplier_number)
+	var multiplier_match: RegExMatch = multiplier.search(seqence_string)
+	var multiplier_number: float = multiplier_match.get_string("multiplier").to_float() if multiplier_match != null else 1.0
+	var without_multiplier: String = seqence_string.substr(multiplier_match.get_string().length()) if multiplier_match != null else seqence_string
+	var cleaned: String = trimmed.sub(without_multiplier, "", true)
+	
+	var track_data: FullPatternData = proccess_pattern(cleaned, multiplier_number)
+	
+	print(track_data.visual_notes)
+
+class FullPatternData:
+	var current_timestamp: float
+	var time_stamps: Array[float]
+	var notes: Array[Note]
+	var visual_notes: Array[String] = []
+	func _init(_current_timestamp: float, _time_stamps: Array[float], _notes: Array[Note]) -> void:
+		current_timestamp = _current_timestamp
+		time_stamps = _time_stamps
+		notes = _notes
+
+static func proccess_pattern(cleaned_pattern: String, multiplier: float = 1.0, octave_offset: int = 0, data: FullPatternData = FullPatternData.new(0.0, [], [])) -> FullPatternData:
+	var notes_and_patterns_matches: Array[RegExMatch] = patterns_and_notes.search_all(cleaned_pattern)
+	
+	for note_or_pattern in notes_and_patterns_matches:
+		
+		var is_note: bool = note_or_pattern.get_string("note") != ""
+		var is_pattern: bool = note_or_pattern.get_string("pattern") != ""
+		var is_rest: bool = note_or_pattern.get_string("rest") != ""
+		
+		if is_note:
+			data.time_stamps.append(data.current_timestamp)
+			var octave: int = note_or_pattern.get_string("note_octave").to_int()
+			var note: Note = Note.build(note_or_pattern.get_string("note_pitch") + str(octave + octave_offset))
+			data.notes.append(note)
+			var note_length: String = note_or_pattern.get_string("note_length")
+			data.current_timestamp += (note_length.to_float() if note_length != "" else 1.0) * multiplier
+			data.visual_notes.append(note_or_pattern.get_string("note"))
+		
+		if is_rest:
+			var rest_length: String = note_or_pattern.get_string("rest_length")
+			data.current_timestamp += (rest_length.to_float() if rest_length != "" else 1.0) * multiplier
+			data.visual_notes.append(note_or_pattern.get_string("rest"))
+		
+		if is_pattern:
+			var sub_pattern: String = note_or_pattern.get_string("pattern")
+			var repititions: int = note_or_pattern.get_string("pattern_repitition").to_int()
+			if repititions == 0: repititions = 1
+			var sub_multiplier: float = note_or_pattern.get_string("pattern_multiplier").to_float()
+			if sub_multiplier == 0: sub_multiplier = 1.0
+			var octave: int = note_or_pattern.get_string("pattern_octave").to_int()
+			for repitition in range(repititions):
+				data = proccess_pattern(sub_pattern, sub_multiplier * multiplier, octave_offset + octave, data)
+	
+	return data
--- content/component/note.gd
+++ content/component/note.gd
@@ -1,92 +1,44 @@
 extends Resource
 class_name Note
 
-var note: NOTE
+var in_octave_scaling: float = 1.0
 var octave: int = 0
 var pitch_scale: float:
 	get():
-		return pitch_scale_from_c(note, octave)
+		return in_octave_scaling * (2 ** octave)
 
-const NOTE_LETTERS: PackedStringArray = ["A", "B", "C", "D", "E", "F", "G", "-"]
+const NOTE_LETTERS: PackedStringArray = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
 
-func _init(_note: NOTE, _octave: int) -> void:
-	note = _note
+## This show ussualy only be used internally in the Note class, or when making custom Note stuff
+func _init(_in_octave_scaling, _octave: int) -> void:
+	in_octave_scaling = _in_octave_scaling
 	octave = _octave
 
-func is_rest() -> bool:
-	return note == Rhythm.NOTE.REST
-
-enum NOTE {
-	A,
-	AS,
-	B,
-	C,
-	CS,
-	D,
-	DS,
-	E,
-	F,
-	FS,
-	G,
-	GS,
-	REST,
-}
-
-static func string_to_note_type(string: String) -> NOTE:
-	match string:
-		"A": return NOTE.A
-		"A#": return NOTE.AS
-		"B": return NOTE.B
-		"C": return NOTE.C
-		"C#": return NOTE.CS
-		"D": return NOTE.D
-		"D#": return NOTE.DS
-		"E": return NOTE.E
-		"F": return NOTE.F
-		"F#": return NOTE.FS
-		"G": return NOTE.G
-		"G#": return NOTE.GS
-		"-": return NOTE.REST
-	assert(false, "String: " + string + " is not a valid note!")
-	return NOTE.C
-
-static func pitch_scale_from_c(note: NOTE, octave: int = 0) -> float:
+static func letter_to_scaling(note_letter: String):
+	assert(note_letter in NOTE_LETTERS, "The note letter must be: " + str(NOTE_LETTERS) + " ONLY! The passed string was: " + note_letter)
 	
-	if note == NOTE.REST: return 0.0
+	const NOTE_CONVERSION = 1.0595
 	
-	const NOTE_UP = 1.0595
-	const NOTE_DOWN = 1.0 / NOTE_UP
+	var scaling_operand: int = -2
 	
-	var octave_convert: float = 2.0 ** octave
-	var note_scale: float = 1.0
+	for letter in NOTE_LETTERS:
+		if note_letter == letter:
+			return NOTE_CONVERSION ** scaling_operand
+		scaling_operand += 1
 	
-	match note:
-		NOTE.A: note_scale = NOTE_DOWN ** 3
-		NOTE.AS: note_scale = NOTE_DOWN ** 2
-		NOTE.B: note_scale = NOTE_DOWN
-		NOTE.C: note_scale = 1.0
-		NOTE.CS: note_scale = NOTE_UP
-		NOTE.D: note_scale = NOTE_UP ** 2
-		NOTE.DS: note_scale = NOTE_UP ** 3
-		NOTE.E: note_scale = NOTE_UP ** 4
-		NOTE.F: note_scale = NOTE_UP ** 5
-		NOTE.FS: note_scale = NOTE_UP ** 6
-		NOTE.G: note_scale = NOTE_UP ** 7
-		NOTE.GS: note_scale = NOTE_UP ** 8
-	
-	return note_scale * octave_convert
+	assert(false, "The note letter must be: A, B, C, D, E, F, or G ONLY! The passed string was: " + note_letter)
 
 static func build(note_string: String) -> Note:
 	assert(note_string.substr(0, 1) in NOTE_LETTERS, "Instruction: \"" + note_string + "\" does not start with a Note Letter! Must be in: " + str(NOTE_LETTERS))
 	
 	if note_string.length() == 1:
-		return Note.new(string_to_note_type(note_string), 0)
+		return Note.new(letter_to_scaling(note_string), 0)
 	if note_string.length() == 2 and note_string.substr(1, 1) == "#":
-		return Note.new(string_to_note_type(note_string), 0)
+		return Note.new(letter_to_scaling(note_string), 0)
 		
 	var sharp_offset: int = 0
 	if note_string.substr(1, 1) == "#": sharp_offset = 1
 	
 	assert(note_string.substr(1 + sharp_offset).is_valid_int(), "The octave of: " + note_string + " is not an integer!")
 	var octave: int = note_string.substr(1 + sharp_offset).to_int()
-	return Note.new(string_to_note_type(note_string.substr(0, 1 + sharp_offset)), octave)
+	return Note.new(letter_to_scaling(note_string.substr(0, 1 + sharp_offset)), octave)
